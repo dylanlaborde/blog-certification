4c2aa47dcc789f94f1b6c0dcf347759d037f5d97
dylanlaborde <dylan4047@gmail.com>
2017-04-05 14:34:07 +0200

    affichage des article sur la page d'acceuil

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Article;
 class HomeController extends Controller
 {
     /**
@@ -23,6 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $article = Article::all();
+       
+        return view('home',['article'=>$article]);
+        
     }
 }
